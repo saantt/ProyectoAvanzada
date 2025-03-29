12f59f50ae11d4622b65bf778418bbb966892e08
MiltonGomez775 <milton1rozo2@gmail.com>
2025-03-28 19:28:23 -0500

    Cambio en Controller ComentarioController.java

--- src/main/java/co/edu/uniquindio/proyectofinal/controller/ComentarioController.java
+++ src/main/java/co/edu/uniquindio/proyectofinal/controller/ComentarioController.java
@@ -10,6 +10,8 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Optional;
 
+//Prueba
+
 @RestController
 @RequestMapping("/comentarios")
 public class ComentarioController {
